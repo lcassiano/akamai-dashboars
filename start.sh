3b0e82d3877bd9bb6ada6f45028443fb700e72c8
lcassiano <leandro@wetware.com.br>
2023-03-14 17:30:07 -0300

    initial version of scripts

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Find kubectl binary in the os path.
+KUBECTL_CMD=$(which kubectl)
+
+# Check if kubectl is installed.
+if [ ! -f "$KUBECTL_CMD" ]; then
+  echo "Kubernetes client (kubectl) is not installed. Please install it to continue!"
+
+  exit 1
+fi
+
+# Check if the kubeconfig file is available.
+if [ ! -f "kubeconfig" ]; then
+  echo "kubeconfig file not found! You can proceed without this file!"
+
+  exit 1
+fi
+
+# Deploy the stack (uncomment the desired deployments and services to be applied).
+export KUBECONFIG=kubeconfig
+
+$KUBECTL_CMD apply -f splunk-deployments.yml
+$KUBECTL_CMD apply -f elastic-deployments.yml
+$KUBECTL_CMD apply -f opensearch-services.yml
