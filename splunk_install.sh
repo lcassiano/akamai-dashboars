08e87ec2311af6c29e5c3806af2951361358d8b2
lcassiano <leandro@wetware.com.br>
2023-03-29 01:52:53 -0300

    new ajusts

--- splunk_install.sh
+++ splunk_install.sh
@@ -19,12 +19,13 @@ fi
 
 # Deploy the stack (uncomment the desired deployments and services to be applied).
 export KUBECONFIG=kubeconfig
-
+export IPADDR=$(/sbin/ifconfig eth0 | awk '/inet / { print $2 }' | sed 's/addr://')
 $KUBECTL_CMD apply -f https://github.com/jetstack/cert-manager/releases/download/v1.11.0/cert-manager.yaml
 sleep 30
 $KUBECTL_CMD apply -f stack-namespaces.yaml
 $KUBECTL_CMD apply -f splunk/stack-deployments.yaml
 $KUBECTL_CMD apply -f splunk/stack-services.yaml
-$KUBECTL_CMD apply -f ssl/letsencrypt-prod.yaml
+while ! $(host $SPLUNKHOSTNAME >/dev/null); do echo Please, configure dns hostname $SPLUNKHOSTNAME to appoint to $IPADDR; sleep 30; done
+sed -e "s|splunk@example.com|$SSLEMAIL|g" ssl/letsencrypt-prod.yaml | $KUBECTL_CMD apply -f -
 $KUBECTL_CMD apply -f ssl/traefik-https-redirect-middleware.yaml
-$KUBECTL_CMD apply -f ssl/ingress-tls.yaml
+sed -e "s|splunk.example.com|$SPLUNKHOSTNAME|g" ssl/ingress-tls.yaml | $KUBECTL_CMD apply -f -
